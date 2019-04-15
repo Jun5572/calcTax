0599bb73e1c255a16768f44ee3e556990d57fb02
J Takiz <takizawa.tab@gmail.com>
2019-04-16 02:06:28 +0900

    configファイル作成

---
+++ app_config.js
@@ -0,0 +1,12 @@
+const MESSAGES = {
+		SUCCESS :{
+			CALC_SUCCESS : "正常に計算できました！"
+		},
+		WARNING :{
+			FULLWIDTH : "半角数字で入力してください！",
+			WRONG_DATA : "数値を入力してください！",
+		},
+		ERROR :{
+			NULL_FORM : "何か値を入力してください！"
+		}
+};
--- index.js
+++ index.js
@@ -1,31 +1,22 @@
-const ERROR_NULL_FORM = "何か値を入力してください！";
-const ERROR_WRONG_DATA = "数値を入力してください！";
-const ERROR_FULLWIDTH = "半角数字を入力してください！"
 
 let calc_button = document.getElementById('calc_button');
 let input = document.getElementById('input');
 let result = document.getElementById('display_result');
 let taxRate = 0.08;
 
-
-
-// エラーを吐いた状態から入力フォームにフォーカスした場合、エラー表示部分のCSSをリセットする。
-input.addEventListener('focus', function(){
-	if ( input.classList =~ "is_error"){
-		input.classList.remove('is_error');
-		result.style.background = 'transparent';
-		result.innerText = '';
-	};
-});
-
-
-// エラーを吐いた時のメッセージとスタイルをセット
+// エラーを吐いた時のメッセージとスタイルをセットする関数
+//		element = ターゲットとなる要素
+//		message = メッセージの種類
+//		className = 付与するクラス名
+//		prop = 操作したいcssのプロパティ
+//		value = プロパティの値
 function setMessageAndStyle( element, message, className, prop, value ){
 	element.innerText = message;
 	element.style[prop] = value;
 	element.classList.add(className);
 };
 
+// クラス追加、削除
 function addClass( element, className ){
 	element.classList.add(className);
 };
@@ -33,63 +24,57 @@ function removeClass( element, className ){
 	element.classList.remove(className);
 };
 
-// 全角文字かどうかを判別(true / false がreturn)
+// 全角文字かどうかを判別(true / false をreturn)
 function checkFullWidthForm( input ){
 	return input.match(/^[^\x01-\x7E\xA1-\xDF]+$/);
 };
 
+// 数値が入力されたときは以下の処理が走る
+function getPriceInTaxRate( input, taxRate ) {
+	// フォームの入力値を10進数に変換
+	let num = parseInt( input, 10 );
+	return Math.floor( num + num * taxRate );
+};
 
-calc_button.addEventListener('click', function(){
-// inputが全角の数値の場合もユーザーに喚起する
-	if( checkFullWidthForm(input.value) ){
-		setMessageAndStyle( result, ERROR_FULLWIDTH, 'error_message', 'background', 'crimson');
-		addClass( input, 'is_error' );
-		return result;
-	};
-// inputが空欄の場合エラーを吐く
-	if( input.value === '' ){
-		setMessageAndStyle( result, ERROR_NULL_FORM, 'error_message', 'background', 'crimson');
-		addClass( input, 'is_error' );
-		return result;
-	};
-
-// inputが数値以外の場合エラーを吐く
-	if( isNaN(input.value)){
-		setMessageAndStyle( result, ERROR_WRONG_DATA, 'error_message', 'background', 'crimson');
+//Enterキーの押下イベント時に発火できるように関数化
+function pushCalcButton() {
+	if( checkFullWidthForm(input.value) ){ // 入力値が全角の場合trueとなりエラーを吐く
+		setMessageAndStyle( result, MESSAGES.WARNING.FULLWIDTH, 'error_message', 'background', 'crimson');
 		addClass( input, 'is_error' );
 		return result;
 	};
 
-
-	// switch (input.value){
-	// 	// 全角文字が入力された場合
-	// 	case checkFullWidthForm(input.value) === true && isNaN(input.value) === true:
-	// 		setMessageAndStyle( result, ERROR_FULLWIDTH, 'error_message', 'background', 'crimson');
-	// 		addClass( input, 'is_error' );
-	// 		return result;
-	// 	// ブランクの場合
-	// 	case input.value === '':
-	// 		setMessageAndStyle( result, ERROR_NULL_FORM, 'error_message', 'background', 'crimson');
-	// 		addClass( input, 'is_error' );
-	// 		return result;
-	// 	// 数値以外
-	// 	case isNaN(input.value) === true:
-	// 		setMessageAndStyle( result, ERROR_WRONG_DATA, 'error_message', 'background', 'crimson');
-	// 		addClass( input, 'is_error' );
-	// 		return result;
-	// };
-
-
-// 正しく数値が入力されたときは以下の処理が走る
-	function getPriceInTaxRate(input, taxRate) {
-		// フォームの入力値を10進数に変換
-		let num = parseInt(input, 10);
-		return Math.floor( num + num * taxRate );
+	if( input.value === '' ){ // 入力値が空欄の場合trueとなりエラーを吐く
+		setMessageAndStyle( result, MESSAGES.ERROR.NULL_FORM, 'error_message', 'background', 'crimson');
+		addClass( input, 'is_error' );
+		return result;
 	};
 
-	let resultCalc = getPriceInTaxRate(input.value, taxRate);
+	if( isNaN(input.value)){ // 入力値が数値以外の場合trueとなりエラーを吐く
+		setMessageAndStyle( result, MESSAGES.WARNING.WRONG_DATA, 'error_message', 'background', 'crimson');
+		addClass( input, 'is_error' );
+		return result;
+	};
+
+
+	let resultCalc = getPriceInTaxRate( input.value, taxRate );
+
 	removeClass( result, 'error_message' );
 	removeClass( input, 'is_error' );
 	result.innerHTML = `${input.value}円の税込価格は、${resultCalc}円です`;
-	input.value = '';
+};
+
+
+// エラーを吐いた状態から入力フォームにフォーカスした場合、エラー表示部分のCSSをリセットする。
+input.addEventListener('focus', function(){
+	if ( input.classList =~ "is_error"){
+		removeClass( input, 'is_error' );
+		result.style.background = 'transparent';
+		input.value = '';
+		result.innerText = '';
+	};
 });
+
+
+// 計算ボタンが押下されたときの処理。Enterキー押下でも発火するようにしたい
+calc_button.addEventListener('click', pushCalcButton );
